74760fe9724e4b42893248d7d039509d6c1fdae4
Joan Pou Barceló <jpou@fundaciobit.org>
2025-04-04 13:41:10 +0200

    en els scripts de postgres, afegeix valors per defecte als ids de les taules, nom a les constants pk i alguns indexs que faltaven #18

--- scripts/bbdd/postgres/create_schema.sql
+++ scripts/bbdd/postgres/create_schema.sql
@@ -17,7 +17,7 @@ create sequence rfh_usuari_seq start 1000 increment 1;
 create sequence rfh_usuarientitat_seq start 1000 increment 1;
 
     create table rfh_activitat (
-       activitatid int8 not null,
+       activitatid int8 DEFAULT nextval('rfh_activitat_seq'::regclass) not null,
         arxiudocumentid varchar(255),
         arxiuexpedientid varchar(255),
         autoritzacioid int8,
@@ -40,11 +40,11 @@ create sequence rfh_usuarientitat_seq start 1000 increment 1;
         tramit varchar(150),
         tramitversio int4,
         url varchar(255),
-        primary key (activitatid)
+        CONSTRAINT rfh_activitat_pk primary key (activitatid)
     );
 
     create table rfh_autoritzacio (
-       autoritzacioid int8 not null,
+       autoritzacioid int8 DEFAULT nextval('rfh_autoritzacio_seq'::regclass) not null,
         cai varchar(255) not null,
         codisia varchar(50) not null,
         datacreacio timestamp not null,
@@ -55,11 +55,11 @@ create sequence rfh_usuarientitat_seq start 1000 increment 1;
         observacions text,
         procediment varchar(255) not null,
         usuariid int8,
-        primary key (autoritzacioid)
+        CONSTRAINT rfh_autoritzacio_pk primary key (autoritzacioid)
     );
 
     create table rfh_digitalib (
-       digitalid int8 not null,
+       digitalid int8 DEFAULT nextval('rfh_digitalib_seq'::regclass) not null,
         datacreacio timestamp,
         entitatid int8 not null,
         fileinfo text,
@@ -71,29 +71,29 @@ create sequence rfh_usuarientitat_seq start 1000 increment 1;
         transactionid int8,
         transactionwebid varchar(255),
         usuariid int8 not null,
-        primary key (digitalid)
+        CONSTRAINT rfh_digitalib_pk primary key (digitalid)
     );
 
     create table rfh_entitat (
-       entitatid int8 not null,
+       entitatid int8 DEFAULT nextval('rfh_entitat_seq'::regclass) not null,
         actiu boolean not null,
         databaixa timestamp,
         nom varchar(255),
         unitatid int8,
-        primary key (entitatid)
+        CONSTRAINT rfh_entitat_pk primary key (entitatid)
     );
 
     create table rfh_fitxer (
-       fitxerid int8 not null,
-        descripcio varchar(1000),
+       fitxerid int8 DEFAULT nextval('rfh_fitxer_seq'::regclass) not null,
+        descripcio varchar(1000) DEFAULT NULL::character varying,
         mime varchar(255) not null,
         nom varchar(255) not null,
         tamany int8 not null,
-        primary key (fitxerid)
+        CONSTRAINT rfh_fitxer_pk primary key (fitxerid)
     );
 
     create table rfh_funcionari (
-       funcionariid int8 not null,
+       funcionariid int8 DEFAULT nextval('rfh_funcionari_seq'::regclass) not null,
         correu varchar(255) not null,
         databaixa timestamp,
         datacreacio timestamp not null,
@@ -106,50 +106,50 @@ create sequence rfh_usuarientitat_seq start 1000 increment 1;
         observacions text,
         tipusidentificador int4 not null,
         usuari varchar(50) not null,
-        primary key (funcionariid)
+        CONSTRAINT rfh_funcionari_pk primary key (funcionariid)
     );
 
     create table rfh_funcionarilloc (
-       funcionarillocid int8 not null,
+       funcionarillocid int8 DEFAULT nextval('rfh_funcionarilloc_seq'::regclass) not null,
         datacreacio timestamp not null,
         datafi date,
         datainici date,
         funcionariid int8 not null,
         llocid int8 not null,
         usuariid int8,
-        primary key (funcionarillocid)
+        CONSTRAINT rfh_funcionarilloc_pk primary key (funcionarillocid)
     );
 
     create table rfh_historic (
-       historicid int8 not null,
+       historicid int8 DEFAULT nextval('rfh_historic_seq'::regclass) not null,
         datacreacio timestamp not null,
         funcionariid int8 not null,
         numerocai varchar(50) not null,
         observacions text,
         usuariid int8,
-        primary key (historicid)
+        CONSTRAINT rfh_historic_pk primary key (historicid)
     );
 
     create table rfh_historiclloc (
-       historicllocid int8 not null,
+       historicllocid int8 DEFAULT nextval('rfh_historiclloc_seq'::regclass) not null,
         datacreacio timestamp not null,
         llocid int8 not null,
         numerocai varchar(50) not null,
         observacions text,
         usuariid int8,
-        primary key (historicllocid)
+        CONSTRAINT rfh_historiclloc_pk primary key (historicllocid)
     );
 
     create table rfh_idioma (
        idiomaid varchar(5) not null,
         nom varchar(50) not null,
         ordre int4 default 0 not null,
-        suportat boolean not null,
-        primary key (idiomaid)
+        suportat boolean DEFAULT true not null,
+        CONSTRAINT rfh_idioma_pk primary key (idiomaid)
     );
 
     create table rfh_lloc (
-       llocid int8 not null,
+       llocid int8 DEFAULT nextval('rfh_lloc_seq'::regclass) not null,
         codilloc varchar(50) not null,
         databaixa timestamp,
         datacreacio timestamp not null,
@@ -158,19 +158,19 @@ create sequence rfh_usuarientitat_seq start 1000 increment 1;
         observacions text,
         personaloamr int4 not null,
         unitatid int8 not null,
-        primary key (llocid)
+        CONSTRAINT rfh_lloc_pk primary key (llocid)
     );
 
     create table rfh_llocrol (
-       llocrolid int8 not null,
+       llocrolid int8 DEFAULT nextval('rfh_llocrol_seq'::regclass) not null,
         datacreacio timestamp not null,
         llocid int8 not null,
         rolid int8 not null,
-        primary key (llocrolid)
+        CONSTRAINT rfh_llocrol_pk primary key (llocrolid)
     );
 
     create table rfh_plugin (
-       pluginid int8 not null,
+       pluginid int8 DEFAULT nextval('rfh_plugin_seq'::regclass) not null,
         actiu boolean not null,
         classe varchar(255),
         datacreacio timestamp,
@@ -179,32 +179,32 @@ create sequence rfh_usuarientitat_seq start 1000 increment 1;
         nom varchar(255) not null,
         properties text,
         tipus varchar(50) not null,
-        primary key (pluginid)
+        CONSTRAINT rfh_plugin_pk primary key (pluginid)
     );
 
     create table rfh_rol (
-       rolid int8 not null,
+       rolid int8 DEFAULT nextval('rfh_rol_seq'::regclass) not null,
         codi varchar(50) not null,
         datacreacio timestamp,
         entitatid int8 not null,
         nomid int8 not null,
-        primary key (rolid)
+        CONSTRAINT rfh_rol_pk primary key (rolid)
     );
 
     create table rfh_traduccio (
-       traduccioid int8 not null,
-        primary key (traduccioid)
+       traduccioid int8 DEFAULT nextval('rfh_traduccio_seq'::regclass) not null,
+       CONSTRAINT rfh_traduccio_pk primary key (traduccioid)
     );
 
     create table rfh_traducciomap (
        traducciomapid int8 not null,
         valor varchar(4000),
-        idiomaid varchar(255) not null,
-        primary key (traducciomapid, idiomaid)
+        idiomaid varchar(10) not null,
+        CONSTRAINT rfh_traducmap_pk primary key (traducciomapid, idiomaid)
     );
 
     create table rfh_unitat (
-       unitatid int8 not null,
+       unitatid int8 DEFAULT nextval('rfh_unitat_seq'::regclass) not null,
         arrel varchar(50),
         arrelversio int4,
         codi varchar(50) not null,
@@ -214,11 +214,11 @@ create sequence rfh_usuarientitat_seq start 1000 increment 1;
         superior varchar(50),
         superiorversio int4,
         versio int4 not null,
-        primary key (unitatid)
+        CONSTRAINT rfh_unitat_pk primary key (unitatid)
     );
 
     create table rfh_usuari (
-       usuariid int8 not null,
+       usuariid int8 DEFAULT nextval('rfh_usuari_seq'::regclass) not null,
         actiu boolean not null,
         correu varchar(255) not null,
         darreraentitat int8,
@@ -230,22 +230,22 @@ create sequence rfh_usuarientitat_seq start 1000 increment 1;
         nif varchar(50) not null,
         nom varchar(255) not null,
         username varchar(255),
-        primary key (usuariid)
+        CONSTRAINT rfh_usuari_pk primary key (usuariid)
     );
 
     create table rfh_usuarientitat (
-       usuarientitatid int8 not null,
+       usuarientitatid int8 DEFAULT nextval('rfh_usuarientitat_seq'::regclass) not null,
         actiu boolean not null,
         entitatid int8 not null,
         usuariid int8 not null,
-        primary key (usuarientitatid)
+        CONSTRAINT rfh_usuarientitat_pk primary key (usuarientitatid)
     );
 create index rfh_activitat_pk_i on rfh_activitat (activitatid);
 create index rfh_activitat_fun_fk_i on rfh_activitat (funcionariid);
 create index rfh_activitat_autoritzaid_fk_i on rfh_activitat (autoritzacioid);
 create index rfh_autoritzacio_pk_i on rfh_autoritzacio (autoritzacioid);
 create index rfh_autoritza_funid_fk_i on rfh_autoritzacio (llocid);
-create index rfh_autoritza_funcionariid_fk_i on rfh_autoritzacio (funcionariid);
+create index rfh_autoritza_funcid_fk_i on rfh_autoritzacio (funcionariid);
 create index rfh_digitalib_pk_i on rfh_digitalib (digitalid);
 create index rfh_digitalib_fitxerid_fk_i on rfh_digitalib (fitxerid);
 create index rfh_digitalib_usuariid_fk_i on rfh_digitalib (usuariid);
@@ -271,7 +271,7 @@ create index rfh_lloc_entitatid_fk_i on rfh_lloc (entitatid);
 create index rfh_lloc_unitatid_fk_i on rfh_lloc (unitatid);
 
     alter table rfh_lloc 
-       add constraint UK_m23ejxgegl5k5e830vdy7jpox unique (codilloc);
+       add constraint rfh_lloc_codilloc_uk unique (codilloc);
 create index rfh_llocrol_pk_i on rfh_llocrol (llocrolid);
 create index rfh_llocrol_llocid_fk_i on rfh_llocrol (llocid);
 create index rfh_llocrol_rolid_fk_i on rfh_llocrol (rolid);
@@ -280,13 +280,15 @@ create index rfh_plugin_entitatid_fk_i on rfh_plugin (entitatid);
 create index rfh_rol_pk_i on rfh_rol (rolid);
 create index rfh_rol_nomid_fk_i on rfh_rol (nomid);
 create index rfh_traduccio_pk_i on rfh_traduccio (traduccioid);
+CREATE INDEX rfh_traducciomap_idiomaid_fk_i ON rfh_traducciomap (idiomaid);
+CREATE INDEX rfh_traducciomap_pk_i ON rfh_traducciomap (traducciomapid);
 create index rfh_unitat_pk_i on rfh_unitat (unitatid);
 create index rfh_usuari_pk_i on rfh_usuari (usuariid);
 create index rfh_usuari_idiomaid_fk_i on rfh_usuari (idiomaid);
 create index rfh_usuari_darreraentitat_fk_i on rfh_usuari (darreraentitat);
 
     alter table rfh_usuari 
-       add constraint UK_bcg3fr6ce1dcpv6i6etn9ban7 unique (username);
+       add constraint rfh_usuari_username_uk unique (username);
 create index rfh_usuarientitat_pk_i on rfh_usuarientitat (usuarientitatid);
 create index rfh_usuarient_entitatid_fk_i on rfh_usuarientitat (entitatid);
 create index rfh_usuarient_usuariid_fk_i on rfh_usuarientitat (usuariid);
@@ -305,7 +307,7 @@ create index rfh_usuarient_usuariid_fk_i on rfh_usuarientitat (usuariid);
        references rfh_funcionari;
 
     alter table rfh_autoritzacio 
-       add constraint rfh_autoritza_funcionari_funcionariid_fk 
+       add constraint rfh_autoritza_funcionari_i_fk 
        foreign key (funcionariid) 
        references rfh_funcionari;
 

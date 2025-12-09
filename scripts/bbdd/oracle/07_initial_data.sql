14571ab1aff262c04188ff46840082a92b56fa69
Joan Pou Barceló <jpou@fundaciobit.org>
2025-12-09 10:11:09 +0100

    corregeix errors de booleans a l'script d'oracle de dades inicials #2
    (cherry picked from commit 5c1d2d22b483045ad0b9c79d18a5e02099cfc06e)

--- scripts/bbdd/oracle/07_initial_data.sql
+++ scripts/bbdd/oracle/07_initial_data.sql
@@ -1,2 +1,2 @@
-INSERT INTO rfh_idioma(idiomaid, nom, suportat, ordre) VALUES ('ca', 'Català', true, 0);
-INSERT INTO rfh_idioma(idiomaid, nom, suportat, ordre) VALUES ('es', 'Castellano', true, 1);
+INSERT INTO rfh_idioma(idiomaid, nom, suportat, ordre) VALUES ('ca', 'Català', 1, 0);
+INSERT INTO rfh_idioma(idiomaid, nom, suportat, ordre) VALUES ('es', 'Castellano', 1, 1);

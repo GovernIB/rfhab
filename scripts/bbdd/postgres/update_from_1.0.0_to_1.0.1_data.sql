017cd70db188bcb9d7f99f1d1b40175b4c309062
Joan Pou Barceló <jpou@ibdigital.caib.es>
2026-01-19 14:03:44 +0100

    actualitza scripts d'inici suprimint dades inicials de prova #2

---
+++ scripts/bbdd/postgres/update_from_1.0.0_to_1.0.1_data.sql
@@ -0,0 +1,4 @@
+delete from rfh_usuarientitat where usuarientitatid = 1;
+delete from rfh_usuari where usuariid = 1;
+delete from rfh_entitat where entitatid = 1;
+delete from rfh_unitat where unitatid = 1;

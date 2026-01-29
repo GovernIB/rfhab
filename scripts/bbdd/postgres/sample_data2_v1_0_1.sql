144e01aa0ee95848a91bbdf5a4da2f0bb945f1c6
Joan Pou Barceló <jpou@ibdigital.caib.es>
2026-01-29 12:54:35 +0100

    afegeix scripts de dades inicials de prova de unitats i de funcionaris necessaris per a la carrega massiva (falta lloc de feina i assignacio del funcionari al lloc de feina) #2

---
+++ scripts/bbdd/postgres/sample_data2_v1_0_1.sql
@@ -0,0 +1,31 @@
+-- Es suposa l'execució prèvia dels scripts 01 fins al 06 i de sample_sata_unitats_v1_0_1.
+
+--
+-- entitatid < 1000
+--
+INSERT INTO rfh_entitat(entitatid, nom, actiu, unitatid, databaixa) VALUES (1, 'Govern de les Illes Balears', true, 1, NULL);
+
+--
+-- usuariid < 1000
+-- actiu: ha de ser 1 per a que l'usuari es consideri actiu
+-- databaixa: ha de ser NULL per a que l'usuari es consideri actiu
+--
+-- TODO: Modificar i posar les dades de qualque usuari existent a keycloak del qual en conegueu les credencials (pot ser qualcun dels vostres usuaris). A més, aquest usuari que poseu hauria de tenir assignats els rols d'aplicació RFH_ADMIN, RFH_USER, RFH_SUPER i RFH_W.
+-- Camps a modificar:
+--  - nom
+--  - llinatge1
+--  - llinatge2
+--  - nif
+--  - correu
+--  - username
+--
+INSERT INTO rfh_usuari(usuariid, nom, llinatge1, llinatge2, nif, correu, actiu, datacreacio, idiomaid, username, darreraentitat, databaixa) VALUES (1, 'NomUsuari', 'RFHAB', NULL, '99999999R', 'rfhab@fundaciobit.org', true, '2024-06-19 14:42:30', 'ca', 'rfhab', 1, NULL);
+
+--
+-- usuarientitatid < 1000
+-- actiu: ha de ser 1 per a que l'usuari es consideri actiu
+--
+INSERT INTO rfh_usuarientitat(usuarientitatid, entitatid, usuariid, actiu) VALUES (1, 1, 1, true);
+
+-- funcionari per a carrega massiva
+INSERT INTO rfh_funcionari(funcionariid, correu, databaixa, datacreacio, entitatid, identificador, llinatge1, llinatge2, nom, numero, observacions, tipusidentificador, usuari) VALUES (1, 'rfhab@fundaciobit.org', NULL, '2026-01-28 09:15:30.001', 1, '99999999R', 'RFHAB', NULL, 'NomUsuari', 'FH_0000001', NULL, 1, 'rfhab');

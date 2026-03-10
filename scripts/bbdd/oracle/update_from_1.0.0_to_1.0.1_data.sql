8649c1672221f341e479aceac789c52c77da319f
fbosch-fundaciobit <fbosch@fundaciobit.org>
2026-03-10 09:54:49 +0100

    Correccions scripts update schema i update data

--- scripts/bbdd/oracle/update_from_1.0.0_to_1.0.1_data.sql
+++ scripts/bbdd/oracle/update_from_1.0.0_to_1.0.1_data.sql
@@ -1,4 +1,4 @@
-delete from rfh_usuarientitat where usuarientitatid = 1;
-delete from rfh_usuari where usuariid = 1;
-delete from rfh_entitat where entitatid = 1;
-delete from rfh_unitat where unitatid = 1;
+delete from RFHAB.RFH_USUARIENTITAT where usuarientitatid = 1;
+delete from RFHAB.RFH_USUARI where usuariid = 1;
+delete from RFHAB.RFH_ENTITAT where entitatid = 1;
+delete from RFHAB.RFH_UNITAT where unitatid = 1;

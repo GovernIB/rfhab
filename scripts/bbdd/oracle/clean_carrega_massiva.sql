14d7761adf00a30c9b6ad00ee3f0151bdffca87c
fbosch-fundaciobit <fbosch@fundaciobit.org>
2026-06-16 15:25:54 +0200

    Update clean carrega massiva

---
+++ scripts/bbdd/oracle/clean_carrega_massiva.sql
@@ -0,0 +1,6 @@
+delete from rfh_historiclloc rhl where rhl.historicllocid > 0;
+delete from rfh_historic rh where rh.historicid > 0;
+delete from rfh_funcionarilloc rfl where rfl.funcionarillocid > 1599;
+delete from rfh_llochabilitacio rlr where rlr.llochabilitacioid > 1479;
+delete from rfh_lloc rl where rl.llocid > 1479;
+delete from rfh_funcionari rf where rf.funcionariid > 3879;
